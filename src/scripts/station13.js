34166160bdf950a3fb7e8fa4a67a4a5f317c1137
Gami <s1320016@u-aizu.ac.jp>
2025-03-03 00:04:42 +0900

    station13 first commit

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  var chb=document.getElementById("check");
+  var txt=document.getElementById("text");
+  if(chb.checked==true){
+    txt.style.backgroundColor="Red";
+  }else{
+    txt.style.backgroundColor="White";
+  }
 }
